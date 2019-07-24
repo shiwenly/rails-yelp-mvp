13f78ccfb71e46b5d0218415c3c4b45fbfeecbbc
steven ly <shiwenly@gmail.com>
2019-07-24 16:53:40 +0800

    ex 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all if Rails.env.development?
+
+5.times do
+  article = Restaurant.new(
+    name: Faker::Food.dish,
+    address: Faker::Address.full_address,
+    phone_number: "#{Faker::PhoneNumber.cell_phone}",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  article.save
+end
